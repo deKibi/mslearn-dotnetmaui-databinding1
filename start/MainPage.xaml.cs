77e69b94ca2f5898683599a19effd2f811f17079
deKibi <devbykibi@gmail.com>
2026-04-22 09:30:15 +0300

    Task 1: Updated MainPage.xaml.cs - no data updated after pressing Refresh button (expected behaviour)

--- start/MainPage.xaml.cs
+++ start/MainPage.xaml.cs
@@ -10,23 +10,15 @@ public partial class MainPage : ContentPage
     private async void btnRefresh_Clicked(object sender, EventArgs e)
     {
         btnRefresh.IsEnabled = false;
+
         actIsBusy.IsRunning = true;
 
-        Models.WeatherData weatherData = await Services.WeatherServer.GetWeather(txtPostalCode.Text);
 
-        lblWind.Text = weatherData.Wind.ToString();
-        lblHumidity.Text = weatherData.Humidity.ToString();
-        lblPrecipitation.Text = weatherData.Precipitation.ToString();
-        lblTemperature.Text = weatherData.Temperature.ToString("#");
+        BindingContext = await Services.WeatherServer.GetWeather(txtPostalCode.Text);
 
-        imgCondition.Source = weatherData.Condition switch
-        {
-            Models.WeatherType.Sunny => ImageSource.FromFile("sunny.png"),
-            Models.WeatherType.Cloudy => ImageSource.FromFile("cloud.png"),
-            _ => ImageSource.FromFile("question.png")
-        };
 
         btnRefresh.IsEnabled = true;
+
         actIsBusy.IsRunning = false;
     }
 }
